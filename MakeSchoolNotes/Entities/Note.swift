0baaa52c7b8076c2a87ab25f4664a71a93ccbaae
Rebecca <rebeccaplease@gmail.com>
2015-06-23 15:57:53 -0400

    Realm database

--- MakeSchoolNotes/Entities/Note.swift
+++ MakeSchoolNotes/Entities/Note.swift
@@ -10,6 +10,8 @@ import Foundation
 import RealmSwift
 
 class Note : Object {
-
+    dynamic var title: String = ""
+    dynamic var content: String = ""
+    dynamic var modificationDate = NSDate()
     
 }
--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -13,10 +13,29 @@ class NotesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var notes: Results<Note>! {
+        didSet {
+            // Whenever notes update, update the table view
+            tableView?.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.dataSource = self
+        let myNote = Note()
+        myNote.title   = "Super Simple Test Note"
+        myNote.content = "A long piece of content"
+        
+        //add myNote to Realm database on each run
+        let realm = Realm() // 1
+        realm.write() { // 2
+            realm.add(myNote) // 3
+            //for deleting all notes
+            //realm.deleteAll()
+        }
+        notes = realm.objects(Note)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,13 +51,21 @@ extension NotesViewController: UITableViewDataSource {
         
         let row = indexPath.row
         //cell.textLabel?.text = "Hello World"
-        cell.titleLabel.text = "Hello"
-        cell.dateLabel.text = "Date"
+        
+        //cell.titleLabel.text = "Hello"
+        //cell.dateLabel.text = "Date"
+        
+        //using Realm
+        let note = notes[row] as Note
+        cell.note = note
+        
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        //return 5
+        //if nil, return 0
+        return Int(notes?.count ?? 0)
     }
     
 }
